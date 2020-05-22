85bbde638d0500098c358551ec553c04ba0c8099
TimothyVerma <65108291+TimothyVerma@users.noreply.github.com>
2020-05-22 18:50:10 +0900

    Update Project.py
    Functional

--- Project.py
+++ Project.py
@@ -1,15 +1,12 @@
 '''
 This is a program to get the weather form openweathermap.org 
 It allows the user to check the weather for multiple locations
-
-EXTRA CREDIT
-Display the Clouds or Winds forecast in terms of partly, mostly cloudy, and calm, breezy, windy.
-
-Still not perfected
-Allow the user to obtain weather forecast data using their zip code and city
 It checks if the location is valid and notifies the user
 It uses try blocks to detect connection errors and notifies the user 
 
+EXTRA CREDIT
+Display the Clouds or Winds forecast in terms of partly, mostly cloudy, and calm, breezy, windy.
+Allow the user to obtain weather forecast data using their zip code and city
 
 Author: 
 Timothy
@@ -18,11 +15,13 @@ commited to git on: May 16 2020
 
 Edited: May 19 2020
 Api integreation started
+
+Edited May 22 2020
+finished?
 '''
 import requests
 from math import floor
 
-data_source = '&appid=5b12de548d5e384e2d222a3104b2c661'
 ##api key for openweathermap.org
     
 def Weather_Check():
@@ -30,125 +29,121 @@ def Weather_Check():
     
     while (True):
     ## Using a loop so you user can querie the weather for multiple cities
+        
+        print('='*80)
 
         print("Enter US zipcode or city name", end = '')
         
         location = input (':: ')
         
         if location.lower == 'exit' or location.lower() == 'end' or location == 'e':
-        
             break
         
         else:
             try:
-        
-                Query_Api( location )
+            ##Attempts to run code that will fail if connection isn't mad
+
+                data = Connect_To_Api( location )
+                ## pulls data from the API
+                
+                Query_Api( data )
+                ##converst the data from the API into a simple sentance. 
+                
+            except:
+                print ("Connection Error")
                 break
-            
-            except Exception as e:
-                print (e)
-        print('='*30)
+        
+        
+def  Connect_To_Api( location ):
+## ci,
 
-## Need to change this completely to make it a valid check for the API            
-# =============================================================================
-# def check_conecction( location ):           
-#     global data_source
-#     
-#     ### A dicttionary of locations
-#     locations = {
-#     '68005' : 'Bellevue',
-#     '90210' : 'Beverly Hills',
-#     }
-#     
-#     ##will check if location is valid, currenly only checkis if it is Bellevue
-#     try: 
-#         if location.title() in locations.values():
-#         
-#             return location.title()
-#         
-#         elif location in locations:
-#             
-#             return locations[location]
-#     
-#     except:
-#         pass
-#     
-#     print ("Sorry, the location entered couldn't be proccessed")
-#     raise ValueError 
-# =============================================================================
+    data_source = '&appid=5b12de548d5e384e2d222a3104b2c661'
 
-def  Query_Api( location ):
-    global data_source
-    
     weather_url = 'http://api.openweathermap.org/data/2.5/weather?q='
-    query = weather_url + location + "&units=imperial" + data_source
-    ## makes a string that is formatted for the API, 
-    ## "&units=imperial" converts the temprature and windspeed data to impreial units
     
+    query =  weather_url + location + ",us" + "&units=imperial" + data_source
+    ## makes a string that is formatted for the API, 
+    ## ',us' makes it only check US codes, otherwise Zipcodes can be misinterpereted 
+    
+    ## "&units=imperial" converts the temprature and windspeed data to impreial units
     #print (query)  ##for debugging, make sure the url is valid 
     
-    data = requests.post(query)
+    data = requests.post( query )
+ 
     if data.status_code == 200:
     ## Checks that the connection was made correctly
-        
-        data_dict = data.json()
-        loc = data_dict['name']
-        ## created a dictionary for the location provided
-        
-        temp = Temp(data_dict)
-        wind = Wind(data_dict)
-        sky = Clouds (data_dict) 
-        
-        print ('It is ' + temp + '°f, ' + wind + ', and ' + sky + ' in ' + loc)
-        
+        return data
     else:
-        print ('Sorry, there is no weather data for ' + location)
+        print ('Sorry, there is no weather data for ' + location )
 
+    
+def Query_Api( data ):
+    ## Checks that the connection if vaild, and runs var
+        
+    data_dict = data.json()
+    loc = data_dict['name']
+    ## created a dictionary for the location provided
+    print('Ok, finding weather data for ' + loc)
+    ##lets user know their location was accepted, converts it to city name
+    
+    temp = Temp( data_dict )
+    wind = Wind ( data_dict )
+    sky = Clouds (data_dict ) 
+    
+    print ('It is ' + temp + '°f, ' + wind + ', and ' + sky + ' in ' + loc)
+    
+   
 def Temp(data_dict):
+    ## Converts the temprature to string
+    
     temp_dict = data_dict['main']
     return str(round(temp_dict['temp']))
         
 def Wind(data_dict):
+    ## converst the wind data to a simple word
     
     wind_dict = data_dict['wind']
     wind_speed = float(wind_dict['speed'])
     
-    #print (wind_speed)
-    ## debugging to see the raw seed
-    wind_types = {        
-        0 : 'calm',
-        1 : 'breezy',
-        2 : 'windy',
-        }
     
     if wind_speed < 30:
+        
+        wind_types = {        
+            0 : 'calm', ##if the wind is below 10mph 
+            1 : 'breezy', ## if the wind is between 10 and 20 mph
+            2 : 'windy', ##if the wind is between 20 and 30 mph
+            }
 
         return wind_types[floor(wind_speed/10)]
     
     else:
 
-        return 'overcast'
-        
+        return 'gusty'
+        ##if the wind is 30mph or more
         
 def Clouds (data_dict):
+    ## Converts the coulds data to a simple word or phrase
+    
     cloud_dict = data_dict['clouds']
     cloud_cover = float(cloud_dict['all'])
     
 
-    clouds = {        
-        0 : 'clear',
-        1 : 'partly cloudy',
-        2 : 'mostly couldy', 
-        }
 
     if cloud_cover < 70:
+        
+        
+        clouds = {        
+            0 : 'clear', ##if there is less than 25% cloud cover
+            1 : 'partly cloudy', ## if there is more than 25% but less than 50%
+            2 : 'mostly couldy', ## if there is more than 50 %
+            }
+        
         return clouds[floor(cloud_cover/25)]
 
     else:
 
         return 'overcast'       
+        ## if there is more than 70% cloud cover
 
-
-if __name__ == '__main__':
-        
+if __name__ == '__main__':     
     Weather_Check()
